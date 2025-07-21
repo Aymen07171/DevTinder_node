31f041e8dd1f2ca64c674b8e55281d77b37352b4
Aymen07171 <elattarayman1@gmail.com>
2025-07-21 18:57:26 +0100

    'Build'

--- src/config/database.js
+++ src/config/database.js
@@ -1,14 +1,25 @@
 const mongoose = require('mongoose');
+const env = require('dotenv');
+env.config()
 
 // Connect to MongoDB
 const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect('mongodb+srv://elattarayman1:ArVVlzRL3dJ11G8Q@devtinder.fr1hqkf.mongodb.net/devtinder?retryWrites=true&w=majority&appName=devtinder');
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
-        return conn;
-    } catch (error) {
-        console.error(`Error connecting to MongoDB: ${error.message}`);
-        process.exit(1); // Exit with failure
+    const conn = await mongoose.connect(process.env.DATABASE);
+ 
+    try{
+
+        if(conn) {
+            
+            console.log("Server is running on port ");
+        }else {
+
+            console.log("Server is not running")
+        }
+
+    }catch(err){
+
+        console.log(err)
+
     }
 }
 
