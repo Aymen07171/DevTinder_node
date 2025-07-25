fc886cf57d07eb1ca17375a1712fe0b3a1da6021
Aymen07171 <elattarayman1@gmail.com>
2025-07-25 22:50:27 +0100

    'Update'

--- src/app.js
+++ src/app.js
@@ -4,90 +4,48 @@ const connectDB = require('./config/database');
 const app = express();
 const cors = require('cors');
 
+
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://54.196.224.91'], // Added your EC2 IP
+  origin: 'http://localhost:5173', // Remplacez par l'URL
   credentials: true, // Permet l'envoi de cookies
-}));
+  }))
 
 // Middlewares - Ordre important
 app.use(express.json());
 app.use(cookieParser());
+
+
 
 // Routes
 // Authentication routes
 const authRouter = require('./routes/auth'); // Importation des routes d'authentification
 app.use('/', authRouter);
 
+
 // Profile routes
+
 const profileRouter = require('./routes/profile'); // Importation des routes de profil
 app.use('/', profileRouter);
 
+
 const requestRouter = require('./routes/request'); // Importation des routes de demande de connexion
-app.use('/', requestRouter);
+app.use('/', requestRouter);  
+
 
 const userRouter = require('./routes/user'); // Importation des routes utilisateur
 app.use('/', userRouter);
 
-// Use environment variable for port, default to 80
-const PORT = process.env.PORT || 80;
 
-connectDB()
+
+connectDB() 
   .then(() => {
     console.log('MongoDB connected');
-    return app.listen(PORT, '0.0.0.0'); // Listen on all interfaces
+    return app.listen(3000);
   })
   .then(() => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log('Server is running on port 3000');
   })
   .catch((err) => {
     console.error('MongoDB connection error:', err);
     process.exit(1); // Arrêt propre de l'application en cas d'erreur
   });
-
-
-
-//   const express = require('express');
-// const cookieParser = require('cookie-parser');
-// const connectDB = require('./config/database');
-// const app = express();
-// const cors = require('cors');
-
-// app.use(cors({
-//   origin: ['http://localhost:5173', 'http://54.196.224.91'], // Added your EC2 IP
-//   credentials: true, // Permet l'envoi de cookies
-// }));
-
-// // Middlewares - Ordre important
-// app.use(express.json());
-// app.use(cookieParser());
-
-// // Routes
-// // Authentication routes
-// const authRouter = require('./routes/auth'); // Importation des routes d'authentification
-// app.use('/', authRouter);
-
-// // Profile routes
-// const profileRouter = require('./routes/profile'); // Importation des routes de profil
-// app.use('/', profileRouter);
-
-// const requestRouter = require('./routes/request'); // Importation des routes de demande de connexion
-// app.use('/', requestRouter);
-
-// const userRouter = require('./routes/user'); // Importation des routes utilisateur
-// app.use('/', userRouter);
-
-// // Use environment variable for port, default to 80
-// const PORT = process.env.PORT || 80;
-
-// connectDB()
-//   .then(() => {
-//     console.log('MongoDB connected');
-//     return app.listen(PORT, '0.0.0.0'); // Listen on all interfaces
-//   })
-//   .then(() => {
-//     console.log(`Server is running on port ${PORT}`);
-//   })
-//   .catch((err) => {
-//     console.error('MongoDB connection error:', err);
-//     process.exit(1); // Arrêt propre de l'application en cas d'erreur
-//   });
